c466329673c0bec8381d92d45953f3abc8dee4c1
Richard Lincoln <r.w.lincoln@gmail.com>
2010-11-08 23:45:16 +0000

    Bumping version for release.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-__author__ = "Richard Lincoln"
-
+import os
 from setuptools import setup, find_packages
 
+# Read the long description from the README.
+thisdir = os.path.abspath(os.path.dirname(__file__))
+f = open(os.path.join(thisdir, "README"))
+kwds = {"long_description": f.read()}
+f.close()
+
 setup(name="PyCIM",
-      description="Python implementation of the Common Information Model.",
-      long_description = open('README').read().strip(),
+      version="14.15.1",
       author="Richard Lincoln",
       author_email="r.w.lincoln@gmail.com",
-      url="http://rwl.github.com/PyCIM",
-      version="14.15.1",
-      license="GNU Lesser General Public License version 2.1",
+      description="Python implementation of the Common Information Model.",
+      license="LGPL",
+      url="http://github.com/rwl/PyCIM",
       include_package_data=False,
       packages=find_packages(),
-      zip_safe=True)
+      zip_safe=True,
+      **kwds)
