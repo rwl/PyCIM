d205577ab328540e8b54fbac0f396c8a843e37a4
Sam Bull <lsb@pocketuniverse.ca>
2017-01-27 22:38:22 -0500

    Fix unicode bug in xml writer affecting Python 2.x

--- PyCIM/Test/RDFXMLWriterTest.py
+++ PyCIM/Test/RDFXMLWriterTest.py
@@ -19,7 +19,10 @@
 # IN THE SOFTWARE.
 
 import unittest
-import io
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 from PyCIM import cimread, cimwrite
 
@@ -38,7 +41,7 @@ class RDFXMLWriterTestCase(unittest.TestCase):
         """
         d = cimread(RDFXML_FILE)
 
-        output = io.StringIO()
+        output = StringIO()
 
         cimwrite(d, output)
 
